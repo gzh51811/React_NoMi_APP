a8a426371aaf98ebf304b3943a7bcd1eb6e36a58
vvcxxc <847731727@qq.com>
2019-04-01 11:04:25 +0800

    ，，en

--- react-project/src/index.js
+++ react-project/src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
 
 
 import {HashRouter,BrowserRoutere} from 'react-router-dom';
+
+let store = createStore((state = {
+   
+}, action) => {
+    switch (action.type) {
+       
+        default:
+        return state
+    }
+})
+
 render(
+    <Provider store={store}>
     <HashRouter>
         <App/>
     </HashRouter>
+    </Provider>
     , document.getElementById('root')
 );
