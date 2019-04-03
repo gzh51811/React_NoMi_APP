b6e155934f50fe80e2653dcb40c529272a300c52
1055041142 <1055041142@qq.com>
2019-04-03 21:22:19 +0800

    更新路由，登录注册静态完成

--- react-project/src/index.js
+++ react-project/src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import {render} from 'react-dom';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
+//引入不需要底部导航组件的路由
+import Reg from './pages/Reg';
+import Login from './pages/Login';
 
+import {HashRouter,Redirect,Route,Switch} from 'react-router-dom';
 
-import {HashRouter,BrowserRoutere} from 'react-router-dom';
 
 let store = createStore((state = {
-   
+
 }, action) => {
     switch (action.type) {
-       
+        
         default:
         return state
     }
@@ -21,8 +24,13 @@ let store = createStore((state = {
 render(
     <Provider store={store}>
         <HashRouter>
-            <App/>
+            <Switch>
+                <Route path="/reg" component={Reg} />
+                <Route path="/login" component={Login}/>
+                <Route path="/" component={App} />
+            </Switch >
         </HashRouter>
     </Provider>
     , document.getElementById('root')
 );
+
