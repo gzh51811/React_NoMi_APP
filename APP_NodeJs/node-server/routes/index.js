bac6e46594bbf2a962531afcc18d7758d1652d08
1055041142 <1055041142@qq.com>
2019-04-08 19:53:59 +0800

    登录功能完成

--- APP_NodeJs/node-server/routes/index.js
+++ APP_NodeJs/node-server/routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 const UserRouter = require('./users');
+const LoginRouter = require('./login');
 
 router.use('/users',UserRouter);
+router.use('/login',LoginRouter);
 
 module.exports = router;
