a324ac0bed2721e8dc444bb1caac3d7daad6580a
qixiong1115 <2830868421@qq.com>
2019-04-11 16:35:39 +0800

    解决请求路径问题和打包

--- react-project/src/hoc/withAxios.js
+++ react-project/src/hoc/withAxios.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'http://106.13.114.93:3001';
 
 // 高阶组件
 export default (Com)=>{
